955170b76f95c90f583f6519e6b1e528e8b6bce5
Rolf Bansbach <rolfisub@gmail.com>
2019-01-02 01:42:10 -0500

    basic cart skeleton

--- src/BasicCart.php
+++ src/BasicCart.php
@@ -4,5 +4,40 @@ namespace Rolfisub\BasicCart;
 
 class BasicCart
 {
+    private $customer;
+    private $items = [];
+
+    /**
+     * @return mixed
+     */
+    public function getCustomer()
+    {
+        return $this->customer;
+    }
+
+    /**
+     * @param mixed $customer
+     */
+    public function setCustomer($customer): void
+    {
+        $this->customer = $customer;
+    }
+
+    /**
+     * @return array
+     */
+    public function getItems(): array
+    {
+        return $this->items;
+    }
+
+    /**
+     * @param array $items
+     */
+    public function setItems(array $items): void
+    {
+        $this->items = $items;
+    }
+
 
 }
